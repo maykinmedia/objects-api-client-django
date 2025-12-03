ed3ed19a646bb3633ea982883cd79ab24b4ecfbf
Paul Schilling <mail@paulschilling.de>
2025-12-03 17:57:56 +0100

    chore: add ape_pie to project dependencies
    The project already had a transitive dependency on ape_pie through
    zgw_consumers. ape_pie is added as first-class dependency to add
    type hints in the Client class.

--- objectsapiclient/services.py
+++ objectsapiclient/services.py
@@ -4,6 +4,7 @@ from urllib.parse import urljoin
 
 from django.core.exceptions import ImproperlyConfigured
 
+from ape_pie import APIClient
 from requests.exceptions import HTTPError
 from zgw_consumers.api_models.base import factory
 from zgw_consumers.client import build_client as build_zgw_client
@@ -29,10 +30,10 @@ class ObjectsAPIService:
                 "Objects API and Objecttypes API"
             )
 
-        self.objects_client = build_zgw_client(
+        self.objects_client: APIClient = build_zgw_client(
             service=self.config.objects_api_service_config
         )
-        self.object_types_client = build_zgw_client(
+        self.object_types_client: APIClient = build_zgw_client(
             service=self.config.object_type_api_service_config
         )
 
